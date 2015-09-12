7916ff513cb9a3c91ce3a1dd39b2e4d8ae299ecf
Nicholas Escalona <nicholas.escalona@gmail.com>
2015-09-12 02:04:38 -0400

    groundwork for overriding vanilla title locs. use parser's csv_rows.

--- build.py
+++ build.py
@@ -28,11 +28,13 @@ def get_cultures(where):
     return cultures
 
 province_loc_files = [
-    'A SWMHcounties.csv', 'A SWMHnewprovinces.csv', 'A SWMHprovinces.csv']
+    'A SWMHcounties.csv', 'A SWMHnewprovinces.csv', 'A SWMHprovinces.csv',
+    'sed_vanilla_override.csv']
 
 def main():
-    templates_loc = sed2path / 'templates/localisation'
-    templates_lt = sed2path / 'templates/common/landed_titles'
+    templates = sed2path / 'templates'
+    templates_loc = templates / 'localisation'
+    templates_lt = templates / 'common/landed_titles'
     build = sed2path / 'SED2'
     build_loc = build / 'localisation'
     build_lt = build / 'common/landed_titles'
@@ -41,28 +43,43 @@ def main():
         shutil.rmtree(str(build), ignore_errors=True)
     build_loc.mkdir(parents=True)
     build_lt.mkdir(parents=True)
+    swmh_files = set()
 
     for inpath in ck2parser.files('localisation/*.csv', basedir=swmhpath):
+        swmh_files.add(inpath.name)
         if no_provinces and inpath.name in province_loc_files:
             continue
         template = templates_loc / inpath.name
         outpath = build_loc / inpath.name
-        with template.open(encoding='cp1252', newline='') as csvfile:
-            sed2 = {row[0].strip():
-                        ' ' if (not row[1].strip() and
-                                re.fullmatch(r'\s+', row[2]))
-                        else row[1].strip()
-                    for row in csv.reader(csvfile, dialect='ckii')}
+        sed2 = {row[0].strip():
+                    ' ' if not row[1].strip() and re.fullmatch(r'\s+', row[2])
+                    else row[1].strip()
+                for row in ck2parser.csv_rows(template)}
         sed2rows = []
-        with inpath.open(encoding='cp1252', newline='') as csvfile:
-            for row in csv.reader(csvfile, dialect='ckii'):
-                if (row[0] and not row[0].startswith('#') and
-                    sed2.get(row[0], True)):
-                    row[1] = sed2.get(row[0], row[1])
-                    row[2:] = [''] * (len(row) - 2)
-                    sed2rows.append(row)
+        for row in ck2parser.csv_rows(inpath):
+            if (row[0] and not row[0].startswith('#') and
+                sed2.get(row[0], True)):
+                row[1] = sed2.get(row[0], row[1])
+                row[2:] = [''] * (len(row) - 2)
+                sed2rows.append(row)
         with outpath.open('w', encoding='cp1252', newline='') as csvfile:
             csv.writer(csvfile, dialect='ckii').writerows(sed2rows)
+
+    # disabled for now
+    # # handle vanilla override files
+    # for template in ck2parser.files('localisation/*.csv', basedir=templates):
+    #     if no_provinces and template.name in province_loc_files:
+    #         continue
+    #     if template.name not in swmh_files:
+    #         outpath = build_loc / template.name
+    #         sed2rows = []
+    #         for row in ck2parser.csv_rows(template):
+    #             if row[0] and not row[0].startswith('#'):
+    #                 row[0], row[1] = row[0].strip(), row[1].strip()
+    #                 row[2:] = [''] * (len(row) - 2)
+    #                 sed2rows.append(row)
+    #         with outpath.open('w', encoding='cp1252', newline='') as csvfile:
+    #             csv.writer(csvfile, dialect='ckii').writerows(sed2rows)
 
     cultures = get_cultures(swmhpath)
     lt_keys = [
@@ -95,15 +112,14 @@ def main():
         template = templates_lt / inpath.with_suffix('.csv').name
         outpath = build_lt / inpath.name
         sed2 = collections.defaultdict(list)
-        with template.open(encoding='cp1252', newline='') as csvfile:
-            for row in csv.reader(csvfile, dialect='ckii'):
-                title, key, val = (s.strip() for s in row[:3])
-                if val:
-                    if key in ['male_names', 'female_names']:
-                        val = ck2parser.Obj.from_iter(
-                            ck2parser.String.from_str(x.strip('"'))
-                            for x in re.findall(r'[^"\s]+|"[^"]*"', val))
-                    sed2[title].append(ck2parser.Pair.from_kv(key, val))
+        for row in ck2parser.csv_rows(template):
+            title, key, val = (s.strip() for s in row[:3])
+            if val:
+                if key in ['male_names', 'female_names']:
+                    val = ck2parser.Obj.from_iter(
+                        ck2parser.String.from_str(x.strip('"'))
+                        for x in re.findall(r'[^"\s]+|"[^"]*"', val))
+                sed2[title].append(ck2parser.Pair.from_kv(key, val))
         tree = ck2parser.parse_file(inpath)
         # from pprint import pprint
         # # pprint(tree.str())
--- make-csvs.py
+++ make-csvs.py
@@ -16,13 +16,13 @@ def get_province_id(where):
     tree = ck2parser.parse_file(where / 'map/default.map')
     defs = next(v.val for n, v in tree if n.val == 'definitions')
     id_name = {}
-    with (where / 'map' / defs).open(newline='', encoding='cp1252') as csvfile:
-        for row in csv.reader(csvfile, dialect='ckii'):
-            try:
-                id_name[int(row[0])] = row[4]
-            except (IndexError, ValueError):
-                continue
+    for row in ck2parser.csv_rows(where / 'map' / defs):
+        try:
+            id_name[int(row[0])] = row[4]
+        except (IndexError, ValueError):
+            continue
     province_id = {}
+    province_title = {}
     for path in ck2parser.files('history/provinces/*.txt', where):
         number, name = path.stem.split(' - ')
         if id_name[int(number)] == name:
@@ -31,8 +31,10 @@ def get_province_id(where):
                 title = next(v.val for n, v in tree if n.val == 'title')
             except StopIteration:
                 continue
-            province_id[title] = 'PROV' + number
-    return province_id
+            the_id = 'PROV' + number
+            province_id[title] = the_id
+            province_title[the_id] = title
+    return province_id, province_title
 
 def get_dynamics(where, cultures, prov_id):
     dynamics = collections.defaultdict(list,
@@ -78,72 +80,95 @@ def main():
     english = collections.defaultdict(str)
     for path in ck2parser.files('English SWMH/localisation/*.csv',
                                 basedir=rootpath):
-        with path.open(newline='', encoding='cp1252') as csvfile:
-            for row in csv.reader(csvfile, dialect='ckii'):
-                try:
+        for row in ck2parser.csv_rows(path):
+            try:
+                if row[0] not in english:
                     english[row[0]] = row[1]
-                except IndexError:
-                    continue
-    prov_id = get_province_id(swmhpath)
+            except IndexError:
+                continue
+    prov_id, prov_title = get_province_id(swmhpath)
     cultures = get_cultures(swmhpath)
     religions, rel_groups = get_religions(swmhpath)
     dynamics = get_dynamics(swmhpath, cultures, prov_id)
     vanilla = ck2parser.localisation()
+    overridden_keys = set()
+    swmh_titles = set()
     prev_loc = collections.defaultdict(str)
     prev_lt = collections.defaultdict(str)
 
     templates = rootpath / 'SED2/templates'
     for path in ck2parser.files('localisation/*.csv', basedir=templates):
-        with path.open(newline='', encoding='cp1252') as csvfile:
-            prev_loc.update({row[0].strip(): row[1].strip()
-                             for row in csv.reader(csvfile, dialect='ckii')
-                             if row[0] and '#' not in row[0]})
+        prev_loc.update({row[0].strip(): row[1].strip()
+                         for row in ck2parser.csv_rows(path)
+                         if row[0] and '#' not in row[0]})
     for path in ck2parser.files('common/landed_titles/*.csv',
                                 basedir=templates):
-        with path.open(newline='', encoding='cp1252') as csvfile:
-            prev_lt.update({(row[0].strip(), row[1].strip()): row[2].strip()
-                            for row in csv.reader(csvfile, dialect='ckii')
-                            if row[0] and '#' not in row[0]})
+        prev_lt.update({(row[0].strip(), row[1].strip()): row[2].strip()
+                        for row in ck2parser.csv_rows(path)
+                        if row[0] and '#' not in row[0]})
 
-    def recurse(v):
-        for n2, v2 in v:
-            if not ck2parser.is_codename(n2.val):
-                continue
-            items = []
-            for n3, v3 in v2:
-                if n3.val in lt_keys:
-                    if isinstance(v3, ck2parser.Obj):
-                        value = ' '.join(s.val for s in v3)
-                    else:
-                        value = v3.val
-                    items.append((n3.val, value))
-            yield n2.val, items
-            yield from recurse(v2)
+    # fill swmh_titles and prov_title before calling
+    def should_override(key):
+        title_match = re.match(r'[ekdcb]_((?!_adj($|_)).)*', key)
+        if title_match is not None:
+            title = title_match.group()
+            if re.fullmatch(r'c_((?!_adj($|_)).)*', key) is not None:
+                return False
+        else:
+            prov_match = re.match(r'PROV\d+', key)
+            if prov_match is not None:
+                try:
+                    title = prov_title[prov_match.group()]
+                except KeyError:
+                    return False
+            else:
+                return False
+        return not title.startswith('b_') and title in swmh_titles
+
+    def recurse(tree):
+        for n, v in tree:
+            if ck2parser.is_codename(n.val):
+                swmh_titles.add(n.val)
+                items = []
+                for n2, v2 in v:
+                    if n2.val in lt_keys:
+                        if isinstance(v2, ck2parser.Obj):
+                            value = ' '.join(s.val for s in v2)
+                        else:
+                            value = v2.val
+                        items.append((n2.val, value))
+                yield n.val, items
+                yield from recurse(v)
 
     with tempfile.TemporaryDirectory() as td:
         templates_t = pathlib.Path(td)
         (templates_t / 'localisation').mkdir(parents=True)
         (templates_t / 'common/landed_titles').mkdir(parents=True)
+        swmh_files = set()
         for inpath in ck2parser.files('localisation/*.csv', basedir=swmhpath):
+            swmh_files.add(inpath.name)
             outpath = templates_t / 'localisation' / inpath.name
             out_rows = [
-                ['#CODE', 'SED2', 'SWMH', 'OTHERSWMH', 'SED1', 'VANILLA']]
+                ['#CODE', 'SED2', 'SWMH', 'OTHER', 'SED1', 'VANILLA']]
             col_width = [0, 8]
-            with inpath.open(newline='', encoding='cp1252') as csvfile:
-                for row in csv.reader(csvfile, dialect='ckii'):
-                    try:
-                        if row[0] and not row[0].startswith('b_'):
-                            if '#' in row[0]:
-                                row = [','.join(row)] + [''] * (len(row) - 1)
-                            out_row = [row[0], prev_loc[row[0]], row[1],
-                                       ','.join(dynamics[row[0]]),
-                                       english[row[0]],
-                                       vanilla.get(row[0], '')]
-                            out_rows.append(out_row)
-                            if '#' not in row[0]:
-                                col_width[0] = max(len(row[0]), col_width[0])
-                    except IndexError:
-                        continue
+            for row in ck2parser.csv_rows(inpath):
+                try:
+                    if row[0] and not '#' in row[0]:
+                        overridden_keys.add(row[0])
+                    if row[0] and not row[0].startswith('b_'):
+                        if '#' in row[0]:
+                            row = [','.join(row)] + [''] * (len(row) - 1)
+                        out_row = [row[0],
+                                   prev_loc[row[0]],
+                                   row[1],
+                                   ','.join(dynamics[row[0]]),
+                                   english[row[0]],
+                                   vanilla.get(row[0], '')]
+                        out_rows.append(out_row)
+                        if '#' not in row[0]:
+                            col_width[0] = max(len(row[0]), col_width[0])
+                except IndexError:
+                    continue
             for i, out_row in enumerate(out_rows):
                 if '#' not in out_row[0] or i == 0:
                     for col, width in enumerate(col_width):
@@ -161,8 +186,7 @@ def main():
                                       basedir=swmhpath):
             outpath = (templates_t / 'common/landed_titles' /
                        inpath.with_suffix('.csv').name)
-            out_rows = [
-                ['#TITLE', 'KEY', 'SED2', 'SWMH']]
+            out_rows = [['#TITLE', 'KEY', 'SED2', 'SWMH']]
             col_width = [0, 0, 8]
             tree = ck2parser.parse_file(inpath)
             for title, pairs in recurse(tree):
@@ -188,6 +212,36 @@ def main():
                     out_row[col] = out_row[col].ljust(width)
             with outpath.open('w', newline='', encoding='cp1252') as csvfile:
                 csv.writer(csvfile, dialect='ckii').writerows(out_rows)
+
+        override_rows = [
+            ['#CODE', 'SED2', 'SWMH', 'OTHER', 'SED1', 'VANILLA']]
+        col_width = [0, 8]
+        for path in ck2parser.files('localisation/*.csv'):
+            if path.name not in swmh_files:
+                override_rows.append(['#' + path.name, '', ''])
+                for row in ck2parser.csv_rows(path):
+                    try:
+                        key, val = row[:2]
+                    except ValueError:
+                        continue
+                    if should_override(key) and key not in overridden_keys:
+                        out_row = [key,
+                                   prev_loc[key],
+                                   '',
+                                   ','.join(dynamics[key]),
+                                   english[key],
+                                   val]
+                        override_rows.append(out_row)
+                        overridden_keys.add(key)
+                        col_width[0] = max(len(key), col_width[0])
+        for i, out_row in enumerate(override_rows):
+            if '#' not in out_row[0] or i == 0:
+                for col, width in enumerate(col_width):
+                    out_row[col] = out_row[col].ljust(width)
+        outpath = templates_t / 'localisation' / 'sed_vanilla_override.csv'
+        with outpath.open('w', newline='', encoding='cp1252') as csvfile:
+            csv.writer(csvfile, dialect='ckii').writerows(override_rows)
+
         while templates.exists():
             print('Removing old templates...')
             shutil.rmtree(str(templates), ignore_errors=True)
