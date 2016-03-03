43b49a7bd677749deb4094aad5547daa1214d49a
Nicholas Escalona <nicholas.escalona@gmail.com>
2016-03-03 12:57:18 -0500

    version bump, miniswmh upstream

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ from print_time import print_time
 
 no_provinces = '--no-provinces' in sys.argv[1:]
 
-version = 'v2.2.0-BETA'
+version = 'v2.2.0'
 if no_provinces:
     version += '-noprovinces'
 
