12de89a8582956a818241a19ccda57bd0019fda6
Nicholas Escalona <nicholas.escalona@gmail.com>
2016-11-17 09:26:16 -0500

    right version dag nabbit

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ from print_time import print_time
 
 no_provinces = '--no-provinces' in sys.argv[1:]
 
-version = 'v2.2.10'
+version = 'v2.2.10-BETA'
 if no_provinces:
     version += '-noprovinces'
 
