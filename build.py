c457a175be73951ec15714ff7b4a61d62f903593
Nicholas Escalona <nicholas.escalona@gmail.com>
2017-03-23 08:46:59 -0400

    .

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ from print_time import print_time
 
 no_provinces = '--no-provinces' in sys.argv[1:]
 
-version = 'v2.2.11'
+version = 'v2.2.12-BETA'
 if no_provinces:
     version += '-noprovinces'
 
