c74cb65fa09772a729610ade5d4ee9b3448bbd97
Matthew D. Hall <zijistark@gmail.com>
2019-04-14 20:40:29 -0700

    version

--- build.py
+++ build.py
@@ -13,7 +13,7 @@ from print_time import print_time
 
 no_provinces = '--no-provinces' in sys.argv[1:]
 
-version = 'v2.2.22-BETA'
+version = 'v2.2.23'
 if no_provinces:
     version += '-noprovinces'
 
